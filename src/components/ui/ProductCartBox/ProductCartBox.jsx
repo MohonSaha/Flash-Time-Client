31e8bd166c407c8192507c1831de4e8b3811a7c2
MohonSaha <practicewithmohon@gmail.com>
2024-08-24 23:47:08 +0600

    implement cart page table row

---
+++ src/components/ui/ProductCartBox/ProductCartBox.jsx
@@ -0,0 +1,80 @@
+import { Button, Checkbox } from "@mui/material";
+import "./ProductCartBox.css";
+import Counter from "../../shared/Counter/Counter";
+import image from "../../../assets/images/p1.jpg";
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined";
+import { useState } from "react";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+const ProductCartBox = () => {
+  const productName =
+    "Symphony v139 Smart Phone RAM 2GB / ROM 32GB - Dual Nano SIM Mobile Phone";
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className="productCartWrapper">
+      <div className="grid grid-cols-12 gap-4">
+        <div className="checkBox col-span-1 flex items-center">
+          <Checkbox {...label} />
+        </div>
+        <div className="productDetails col-span-6 flex items-center">
+          <div className="flex">
+            <img src={image} alt="" />
+            <div className="productInfo">
+              <h5>
+                <p className="inline">
+                  {expanded ? productName : `${productName.slice(0, 34)}... `}
+                  <span
+                    onClick={toggleExpand}
+                    className="toggleExpandButton cursor-pointer text-slate-400"
+                  >
+                    {expanded ? (
+                      <>
+                        <span>
+                          see less <KeyboardArrowUpOutlinedIcon />
+                        </span>
+                      </>
+                    ) : (
+                      "see more"
+                    )}
+                  </span>
+                </p>
+              </h5>
+              <p className="clasification">
+                Symphony, <span>Storage Capacity: 128GB</span>,
+                <span> Color Family: Forest green</span>
+              </p>
+              <p className="available">Only 6 item(s) in stock</p>
+              <div className="offerBox">
+                <LocalOfferOutlinedIcon />
+                <span>30% Flat Off</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 flex items-center priceWrapper">
+          <div>
+            <h2>$260</h2>
+            <div>
+              <DeleteForeverOutlinedIcon />
+            </div>
+          </div>
+        </div>
+        <div className="col-span-2 flex items-center justify-center">
+          <Counter />
+        </div>
+        <div className="col-span-1 flex items-center">Price</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCartBox;
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import "./Cart.css";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { Button } from "@mui/material";
+import ProductCartBox from "../../components/ui/ProductCartBox/ProductCartBox";
 
 const Cart = () => {
   const breadcrumbsMenu = [
@@ -33,25 +34,32 @@ const Cart = () => {
       <section className="cartSection">
         <div className="container-fluid">
           <div className="grid grid-cols-12">
-            <div className="col-span-7">
+            <div className="col-span-8">
               <div>
                 <h1 className="hd">Your Cart</h1>
-                <div className="flex">
+                <div className="flex cartHeader">
                   <p className="cartNumber">
                     There are <span className="text-flash">0</span> products in
                     your cart
                   </p>
                   <div className="clearCart ml-auto">
-                    <Button className="btn-outline-gray">
-                      <DeleteOutlinedIcon />
-                      Clear Cart
-                    </Button>
+                    <DeleteOutlinedIcon />
+                    Clear Cart
+                  </div>
+                </div>
+              </div>
+
+              <div className="cartWrapper">
+                <div>
+                  <div className="companyWrapper"></div>
+                  <div className="">
+                    <ProductCartBox />
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="col-span-5"></div>
+            <div className="col-span-4"></div>
           </div>
         </div>
       </section>
